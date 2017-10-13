023ce1a25302135da9e795463d6854079e784942
JunxianXie <xie.junxian@yahoo.com>
2017-10-13 16:24:08 +0200

    Excercice 1

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,20 @@
 package exercise1;
 
 class Person {
+	private int age;
+	private String name;
+
+	public int getAge() {
+
+		return age;
+	}
+
+	public String getname(){
+		return name;
+	}
+
+	public Person(String name, int age){
+		this.age=age;
+		this.name=name;
+	}
 }
